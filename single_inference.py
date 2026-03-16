1eb2b63a080eb7977bcc28836ba949e0ccb78b45
kirby-lu <lp.oreo.1215@gmail.com>
2026-03-16 20:46:20 +0800

    [fix] 修改names

--- single_inference.py
+++ single_inference.py
@@ -12,7 +12,19 @@ model = attempt_load_weights("./weights/yolov8s.pt")
 INPUT_W=640
 INPUT_H=640
 
-names = ["person","cat","dog","horse","horse","car"]
+names = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball",
+    "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+    "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
+    "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop",
+    "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink",
+    "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier",
+    "toothbrush"
+]
 
 # 前处理和YOLOv5相同
 def preprocess_image(image_path):
